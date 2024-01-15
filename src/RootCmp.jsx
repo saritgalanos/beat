0ec47beb4b386de3d50da161e364e2136b475b71
Sarit Galanos <saritgalanos@gmail.com>
2024-01-15 21:38:10 +0200

    fix attempt to deploy to git pages

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Routes, Route } from 'react-router'
-
-//import routes from './routes'
 import { BeatIndex } from './pages/BeatIndex'
 import { SearchPage } from './pages/SearchPage'
 import { HomePage } from './pages/HomePage'
